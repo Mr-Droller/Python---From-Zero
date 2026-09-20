a4670d5357b82bcbb2a6e5f6600fd2321658bc02
Droller <drollercanvas@gmail.com>
2026-09-20 22:50:46 +0545

    Add files via upload
    Reverse String and Palindrome Checker done through Recursion technique

---
+++ recursive_palindromec_checker.py
@@ -0,0 +1,18 @@
+def reverse_string(string):
+    if len(string) == 0:
+        return ""
+    else:
+        return string[-1] + reverse_string(string[:-1])
+
+
+def palindrome_checker(string):
+    temp = reverse_string(string)
+    if temp == string:
+        return True
+    else:
+        return False
+
+
+string = "racecar"
+result = palindrome_checker(string)
+print(result)
