158f6dc150e1fe1323c0fbf2e7c14e4cbf5d0537
Droller <drollercanvas@gmail.com>
2026-09-20 22:58:20 +0545

    Add files via upload
    Exponentiation done through Recursion.

---
+++ recursive_exponentiation.py
@@ -0,0 +1,9 @@
+def exponentiation(number, base):
+    if base == 0:
+        return 1
+    else:
+        return number * exponentiation(number, base - 1)
+
+
+result = exponentiation(2, 1)
+print(result)
